3ebef47756bb7a97673eb59cdcaee3101ff9884e
Lalu Fatan <lalufatan25@gmail.com>
2026-01-02 16:55:21 +0700

    menambahkan view model di HomeViewModel.kt

--- app/src/main/java/com/example/pertemuan14/view/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/view/viewmodel/HomeViewModel.kt
@@ -1,2 +1,39 @@
 package com.example.pertemuan14.view.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14.modeldata.Siswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositorySiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: Exception) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
