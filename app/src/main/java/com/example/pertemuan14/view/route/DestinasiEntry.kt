bb174cf647cf1744b34da89b80481c4b0f735f79
Lalu Fatan <lalufatan25@gmail.com>
2026-01-02 16:50:29 +0700

    feat: implementasi DestinasiNavigasi pada DestinasiEntry

--- app/src/main/java/com/example/pertemuan14/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/pertemuan14/view/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.pertemuan14.view.route
 
-object DestinasiEntry {
+import com.example.pertemuan14.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
