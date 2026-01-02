c9fc228cb642b0cd4f263519b73e8599127e9358
Lalu Fatan <lalufatan25@gmail.com>
2026-01-02 16:50:52 +0700

    implementasi DestinasiNavigasi di DestinasiHome

--- app/src/main/java/com/example/pertemuan14/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/pertemuan14/view/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.pertemuan14.view.route
 
-object DestinasiHome {
+import com.example.pertemuan14.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
