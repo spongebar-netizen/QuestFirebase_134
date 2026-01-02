365473c7c68ac4103ee4eb9077ae68e2e5833c82
Lalu Fatan <lalufatan25@gmail.com>
2026-01-02 16:00:08 +0700

    menambahkan container aplikasi dan repositori siswa

--- app/src/main/java/com/example/pertemuan14/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan14/repositori/ContainerApp.kt
@@ -1,2 +1,22 @@
 package com.example.pertemuan14.repositori
 
+import android.app.Application
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
