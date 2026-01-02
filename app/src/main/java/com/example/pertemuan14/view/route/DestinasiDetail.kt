98b90ea8ace1ebc27409c78f9e88c008c47d04c3
Lalu Fatan <lalufatan25@gmail.com>
2026-01-02 16:50:06 +0700

    menambahkan destinasi detail

--- app/src/main/java/com/example/pertemuan14/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/pertemuan14/view/route/DestinasiDetail.kt
@@ -1,4 +1,12 @@
 package com.example.pertemuan14.view.route
 
-object DestinasiDetail {
+import com.example.pertemuan14.R
+
+
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
