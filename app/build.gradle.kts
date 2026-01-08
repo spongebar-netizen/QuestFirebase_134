5c63a23472c63a562b54bb3696df86a505b12c66
Lalu Fatan <lalufatan25@gmail.com>
2026-01-08 17:12:50 +0700

    menambahkan halaman Detail untuk menampilkan detail siswa

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,6 @@ android {
 dependencies {
 
     implementation(libs.navigation.compose)
-    //viemodel
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
     implementation(libs.androidx.core.ktx)
@@ -56,6 +55,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,5 +69,6 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.analytics)
     implementation(libs.coroutines.play.services)
+    implementation("androidx.compose.material:material-icons-extended")
 
 }
