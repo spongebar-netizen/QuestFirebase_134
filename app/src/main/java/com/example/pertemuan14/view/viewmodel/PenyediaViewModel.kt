2efecfc96332fce1bd6665589682c7e6f10cdec0
Lalu Fatan <lalufatan25@gmail.com>
2026-01-08 17:12:21 +0700

    menambahkan DetailViewModel dan EditViewModel

--- app/src/main/java/com/example/pertemuan14/view/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/view/viewmodel/PenyediaViewModel.kt
@@ -1,17 +1,43 @@
 package com.example.pertemuan14.view.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan14.repositori.AplikasiDataSiswa
+import com.example.pertemuan14.view.viewmodel.DetailViewModel
+import com.example.pertemuan14.view.viewmodel.EditViewModel
+import com.example.pertemuan14.view.viewmodel.EntryViewModel
+import com.example.pertemuan14.view.viewmodel.HomeViewModel
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
-        )
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as AplikasiDataSiswa)
+
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
